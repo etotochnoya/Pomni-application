b44906814400080217bb3ea6bbab1f8b101aaa38
Obi-Van-Cenobi <gim2003gim2003gim@gmail.ru>
2024-01-08 22:31:46 +0300

    Changed fastTyping.js to get data from backend

--- src/main/resources/static/fastTyping.js
+++ src/main/resources/static/fastTyping.js
@@ -5,38 +5,52 @@ const restartBtn = document.getElementById("restart-button")
 const MAX_SHOW_TEXT_SIZE = 11
 
 let index = 0
-let text = getText()
+getText = async () => {
+  const url = 'http://localhost:8080/fastTyping/data';// here s your loccalhost 9000....))
 
-inputDiv.textContent = text.substring(0, MAX_SHOW_TEXT_SIZE)
-
-
-document.onkeypress = function (event) {
-    event = event || window.event;
-    if(text[index] === event.key){
-        index++
-        alreadyInputDiv.textContent += event.key
-        if(alreadyInputDiv.textContent.length > MAX_SHOW_TEXT_SIZE){
-            alreadyInputDiv.textContent = alreadyInputDiv.textContent.substring(alreadyInputDiv.textContent.length - MAX_SHOW_TEXT_SIZE, alreadyInputDiv.textContent.length)
-        }
-
-        inputDiv.textContent = text.substring(index, MAX_SHOW_TEXT_SIZE + index)
-    }
+  const textResponse = await makeRequest('GET', url);
+  return JSON.parse(textResponse).text;
 }
 
-function getText(){
-    const Http = new XMLHttpRequest();
-    const url = 'http://localhost:8080/fastTyping/data';// here s your loccalhost 9000....))
-    Http.open("GET", url);
-    Http.send();
+getText()
+  .then(text => {
+    inputDiv.textContent = text.substring(0, MAX_SHOW_TEXT_SIZE)
 
-    Http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) { // check that all is no go to hell))
-            console.log(Http.responseText);
-            var response = JSON.parse(this.responseText);
-            var text = response.text;
-            console.log(text);
+    document.onkeypress = function (event) {
+        event = event || window.event;
+        if(text[index] === event.key){
+            index++
+            alreadyInputDiv.textContent += event.key
+            if(alreadyInputDiv.textContent.length > MAX_SHOW_TEXT_SIZE){
+                alreadyInputDiv.textContent = alreadyInputDiv.textContent.substring(alreadyInputDiv.textContent.length - MAX_SHOW_TEXT_SIZE, alreadyInputDiv.textContent.length)
+            }
+
+            inputDiv.textContent = text.substring(index, MAX_SHOW_TEXT_SIZE + index)
         }
-      }
+    }
+  })
+  .catch(err => console.log(err));
 
-    return 'rrtyuiop[]    qwqw dsfaasdfhjsah      hsk  ' // just mock))
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
 }
